f7b4ec384b10a217d7b5dd8227ee87074979cfa4
Michael Opper Moser <waiting4tzeentch@gmail.com>
2016-02-01 12:28:22 -0500

    Completed Challenge 1.6 "Not for Sale"
    Tutorial located:
    http://campus.codeschool.com/courses/shaping-up-with-angular-js/level/1/section/3/not-for-sale

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
-(function(){
-	var gem = { name: 'Azurite', price: 2.95 };
-	var app = angular.module('gemStore', []);
+(function() {
+  var app = angular.module('gemStore', []);
 
-	app.controller('StoreController', function(){
-		this.product=gem;
-	});
+  app.controller('StoreController', function(){
+    this.product = gem;
+  });
 
+  var gem = {
+    name: 'Azurite',
+    price: 110.50,
+    canPurchase: false,
+    soldOut: true
+  };
 })();
