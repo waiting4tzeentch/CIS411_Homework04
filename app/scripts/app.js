9129b6db8538eedb380f675852c716be2fca782c
Michael Opper Moser <michael@moserism.com>
2016-03-06 15:45:30 -0500

    completed level 5.
    Had trouble with the json file. Something went wrong when I copied my stuff
    over to it.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,12 +2,15 @@
   var app = angular.module('gemStore', ['store-directives']);
 
   app.controller('StoreController', ['$http',function($http){
-    var store = this;
-    store.products = [];
-    $http.get('/json/store-products.json').success(function(data){
-      store.products=data;
-    });
-  }]);
+		var store = this;
+		
+		store.products = [];
+		
+		$http.get('/json/store-products.json').success(function(data){
+			store.products = data;
+		});
+		
+	}]);
 
   
    app.controller('ReviewController', function(){
